64374eaa4dfafe62ea8a189ba686da882b7babee
lt9e <lt9e@fresnou.org>
2023-04-12 14:17:30 -0700

    initial commit

--- MadLibs.js
+++ MadLibs.js
@@ -1,6 +1,6 @@
 const readline=require('readline-sync');
 
-let word=readline.question("noun: ");
+let word=readline.question("Costco: ");
 
 let myList=[ ];
 myList.push(readline.question('House: ');
