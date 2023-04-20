5a0489737821266e039958a7a2372d6e9c14fe81
lt9e <lt9e@fresnou.org>
2023-04-20 14:11:30 -0700

    initial commit

--- CharCreator.js
+++ CharCreator.js
@@ -1,12 +1,13 @@
 const readline = require('readline-sync');
 
-let diceRolled = [];
+let diceRolled = []
+let eyeColors = ['blue','green','brown','hazel','black'];
+let Race = ['Human','Fairy','Goblin','Goliath','Gnome','Elf','Dwarf','Minotaur','Orc'];
 let character1 = {
-    //empty for now
+//empty to start
 }
 
 createChar(character1, diceRolled);
-
 function rolld6(){
     return Math.ceil(Math.random()*6)
 }
@@ -33,8 +34,19 @@ function rollStat(list){
     return sum(list);
 }
 
+function pickRandomChoice(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum];
+}
+
+function pickRandomChoice2(choices){
+    let randomNum = Math.floor(Math.random()*choices.length)
+    return choices[randomNum];
+}
 function createChar(character, list){
     character.name = readline.question("Enter Character Name: ");
+    character.eyeColor = pickRandomChoice(eyeColors);
+    character.Race = pickRandomChoice2(Race);
     console.log("Rolling Strength")
     character.strength = rollStat(diceRolled);
     console.log("Rolling Dexterity")
@@ -47,16 +59,21 @@ function createChar(character, list){
     character.wisdom = rollStat(diceRolled);
     console.log("Rolling Charisma")
     character.charisma = rollStat(diceRolled);
+    character.luck = rolld6();
     console.log(character)
 }
 
 function displayChar(char){
     console.log('-------------------------');
     console.log(`Name: ${char.name}`);
+    console.log(`Race: ${char.race}`);
+    console.log(`Eye Color: ${char.eyeColor}`);
     console.log(`STR: ${char.strength}`);
     console.log(`DEX: ${char.dexterity}`);
     console.log(`CON: ${char.constitution}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
     console.log(`CHA: ${char.charisma}`);
+    console.log(`LUCK: ${char.luck}`);
+    console.log('-------------------------------');
 }
