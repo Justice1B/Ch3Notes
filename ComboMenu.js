426bae38ab3aa990a38f2bfbd4026a602046be35
lt9e <lt9e@fresnou.org>
2023-04-26 14:40:14 -0700

    initial commit

--- ComboMenu.js
+++ ComboMenu.js
@@ -1,14 +1,40 @@
 const readline = require("readline-sync");
+let items = [];
 
-let menu = ['Spicy Chicken Sandwich: $4.50', 'Cheeseburger: $1.50', 'Hamburger: $1.00', 'Chicken Sandwich: $2.50']
+let menu = [
+    'GrilledCheese: $2.50',
+    'Cheeseburger: $1.50',
+    'Hamburger: $1.00',
+    'ChickenSandwich: $2.00'
+]
 console.log(`${menu}`);
-let beverage = ['Pepsi', 'Sprite', 'Coca-Cola', 'Coffee', 'Water']
+
+let beverage = [
+    'Pepsi',
+    'Sprite',
+    'CocaCola',
+    'Coffee',
+    'Water'
+]
 console.log(`${beverage}`);
-let beverageSizes = ['small $1.00', 'medium $1.75', 'large $2.25']
+
+let beverageSizes = [
+    'small: $1.00',
+    'medium: $1.75',
+    'large: $2.25'
+]
 console.log(`${beverageSizes}`);
+
 let answer1 = readline.question("What type of sandwich or burger would you like? \n Your Answer: ")
+items.push()
 let answer2 = readline.question("Would you like a beverage? \n Your Answer: ")
+items.push()
 
 if(answer2 == "yes"){
     let answer3 = readline.question("What beverage size would you like? \n Your Answer: ")
+    items.push()
+}
+
+for (let i=0, i<items.length; i++){
+    console.log(`You ordered ${items[i]}`);
 }
