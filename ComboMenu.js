83402dea8402de68d80683da1c0b704f134668dc
lt9e <lt9e@fresnou.org>
2023-04-28 14:18:17 -0700

    initial commit

--- ComboMenu.js
+++ ComboMenu.js
@@ -1,18 +1,17 @@
 const readline = require("readline-sync");
 let items = [];
+let cost =[];
 
 let menu = [
-    'GrilledCheese: $2.50',
-    'Cheeseburger: $1.50',
+    'Burger: $1.50',
+    'Cheeseburger: $2.00',
     'Hamburger: $1.00',
-    'ChickenSandwich: $2.00'
 ]
 console.log(`${menu}`);
 
 let beverage = [
     'Pepsi',
     'Sprite',
-    'CocaCola',
     'Coffee',
     'Water'
 ]
@@ -26,15 +25,34 @@ let beverageSizes = [
 console.log(`${beverageSizes}`);
 
 let answer1 = readline.question("What type of sandwich or burger would you like? \n Your Answer: ")
-items.push()
+items.push(answer1)
+if(answer1 == 'Burger') {
+    cost.push(1.50)
+}else if(answer1 == 'Cheeseburger'){
+    cost.push(2.00)
+}   else {
+    cost.push(1.00)
+}
+
 let answer2 = readline.question("Would you like a beverage? \n Your Answer: ")
-items.push()
+items.push(answer2)
 
 if(answer2 == "yes"){
     let answer3 = readline.question("What beverage size would you like? \n Your Answer: ")
     items.push()
+    console.log(answer3)
+
+} else {
+    console.log("Didn't choose drink")
 }
 
-for (let i=0, i<items.length; i++){
-    console.log(`You ordered ${items[i]}`);
+if(answer2 == 'small') {
+    cost.push(1.00)
+} else if(answer2 == 'medium') {
+    cost.push(1.75)
+} else if(answer2 == 'large') {
+    cost.push(2.25)
+} else {
+    cost.push(0.00)
 }
+console.log(cost)
